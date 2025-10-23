7e70fc6fa5891c394fa0c7bcf68bc35803909d7d
yuheng zhang <670226417@qq.com>
2025-10-23 19:53:14 +1300

    solve the problem about audio

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            Debug.Log("âœ?AudioManager initialized (no audio)");
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+    public void PlaySound(string soundName)
+    {
+        
+    }
+
+    public void PlayPowerModeMusic()
+    {
+        
+    }
+
+    public void StopPowerModeMusic()
+    {
+        
+    }
+
+    public void StopMusic()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+}
